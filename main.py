d7d68bc68f625be1d9cb2edff59ff12e0be7943e
rafael_bit <rafael.verde88@gmail.com>
2021-01-20 17:25:57 -0300

    subindo mais uma azeitada

--- main.py
+++ main.py
@@ -26,22 +26,24 @@ print('Saldo Inicial BRL: '+ str(round(saldo_brl_inicial,1)))
 print('Saldo Inicial Cripto: '+ str(round(saldo_cripto_inicial,1)))
 
 while i <= 20000:
+    try:
+        # Instancia das corretoras por ativo
+        mercadoBitcoin = Corretora('MercadoBitcoin', ativo)
+        brasilBitcoin = Corretora('BrasilBitcoin', ativo)
 
-    # Instancia das corretoras por ativo
-    mercadoBitcoin = Corretora('MercadoBitcoin', ativo)
-    brasilBitcoin = Corretora('BrasilBitcoin', ativo)
-
-    retornoCompra = Arbitragem.run(mercadoBitcoin, brasilBitcoin, ativo, True)
-    retornoVenda = Arbitragem.run(brasilBitcoin, mercadoBitcoin, ativo, True)   
-
-    mercadoBitcoin.atualizarSaldo()
-    brasilBitcoin.atualizarSaldo()
-
-    i += 1
+        retornoCompra = Arbitragem.run(mercadoBitcoin, brasilBitcoin, ativo, True)
+        retornoVenda = Arbitragem.run(brasilBitcoin, mercadoBitcoin, ativo, True)   
+        
+    except Exception as erro:
+        print(erro)
 
     if retornoCompra['sucesso'] or retornoVenda['sucesso']:
+
+        mercadoBitcoin.atualizarSaldo()
+        brasilBitcoin.atualizarSaldo()
 
         print('Total PnL BRL: '+ str(round(mercadoBitcoin.saldoBRL+brasilBitcoin.saldoBRL-saldo_brl_inicial,1)))
         print('Total PnL Cripto: '+ str(round(mercadoBitcoin.saldoCrypto+brasilBitcoin.saldoCrypto-saldo_cripto_inicial,1)))
 
+    i += 1
     time.sleep(45)
