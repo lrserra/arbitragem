3bf8f45ce25caf8fecc3e90884d0b46e6b6f1be8
rafael_bit <rafael.verde88@gmail.com>
2021-01-25 12:42:06 -0300

    bug

--- leilao.py
+++ leilao.py
@@ -9,35 +9,33 @@ class Leilao:
 
         #ja considerando que cancelou as ordens
         logList = {'sucesso': False, 'idOrdem': 0 }
+        
+        qtd_de_moedas = len(Util.obter_lista_de_moedas())
 
-        if idOrdem==0:
+        corretoraParte.atualizarSaldo()
+        corretoraContraparte.atualizarSaldo()
+
+        saldoTotalBRL = corretoraParte.saldoBRL + corretoraContraparte.saldoBRL
+        
+        #corretoraParte tem que ser Brasil, pq la a liquidez é menor e mais facil de fazer leilão
+        if ((corretoraParte.precoCompra-0.01) >= 1.01 * corretoraContraparte.precoCompra):# o preço que eu posso vender é maior doq o preço que posso comprar
             
-            qtd_de_moedas = len(Util.obter_lista_de_moedas())
+            gostaria_de_vender = corretoraParte.saldoCrypto/4
+            maximo_que_consigo_zerar = corretoraContraparte.saldoBRL/(qtd_de_moedas*corretoraContraparte.precoCompra*1.01)
+            qtdNegociada = min(gostaria_de_vender,maximo_que_consigo_zerar)
 
-            corretoraParte.atualizarSaldo()
-            corretoraContraparte.atualizarSaldo()
-
-            saldoTotalBRL = corretoraParte.saldoBRL + corretoraContraparte.saldoBRL
-            
-            #corretoraParte tem que ser Brasil, pq la a liquidez é menor e mais facil de fazer leilão
-            if ((corretoraParte.precoCompra-0.01) >= 1.01 * corretoraContraparte.precoCompra):# o preço que eu posso vender é maior doq o preço que posso comprar
+            if (qtdNegociada>0) and (corretoraContraparte.saldoBRL>1):#nao pode ter saldo na mercado de menos de um real
                 
-                gostaria_de_vender = corretoraParte.saldoCrypto/4
-                maximo_que_consigo_zerar = corretoraContraparte.saldoBRL/(qtd_de_moedas*corretoraContraparte.precoCompra*1.01)
-                qtdNegociada = min(gostaria_de_vender,maximo_que_consigo_zerar)
+                if corretoraParte.saldoBRL < (saldoTotalBRL/8): #eh pra ser deseperado aqui, tenho menos em reais doq um oitavo do totalbrl
+                    #quando estou desesperado uso a regra do pnl zero
+                    corretoraParte.precoVenda = 0.01+corretoraContraparte.precoCompra*1.01 #pnl zero
+                    vendaCorretoraParte = corretoraParte.enviarOrdemVenda(qtdNegociada, 'limited') 
+                    logList['idOrdem'] = vendaCorretoraParte['data']['id']
 
-                if (qtdNegociada>0) and (corretoraContraparte.saldoBRL>1):#nao pode ter saldo na mercado de menos de um real
-                    
-                    if corretoraParte.saldoBRL < (saldoTotalBRL/8): #eh pra ser deseperado aqui, tenho menos em reais doq um oitavo do totalbrl
-                        #quando estou desesperado uso a regra do pnl zero
-                        corretoraParte.precoVenda = 0.01+corretoraContraparte.precoCompra*1.01 #pnl zero
-                        vendaCorretoraParte = corretoraParte.enviarOrdemVenda(qtdNegociada, 'limited') 
-                        logList['idOrdem'] = vendaCorretoraParte['data']['id']
-
-                    else:#todos outros casos
-                        corretoraParte.precoVenda = corretoraParte.precoCompra - 0.01 #quando não estou desesperado, pnl maximo
-                        vendaCorretoraParte = corretoraParte.enviarOrdemVenda(qtdNegociada, 'limited') 
-                        logList['idOrdem'] = vendaCorretoraParte['data']['id']
+                else:#todos outros casos
+                    corretoraParte.precoVenda = corretoraParte.precoCompra - 0.01 #quando não estou desesperado, pnl maximo
+                    vendaCorretoraParte = corretoraParte.enviarOrdemVenda(qtdNegociada, 'limited') 
+                    logList['idOrdem'] = vendaCorretoraParte['data']['id']
 
         return logList
 
@@ -46,34 +44,32 @@ class Leilao:
         #ja considerando que cancelou as ordens
         logList = {'sucesso': False, 'idOrdem': 0 }
 
-        if idOrdem != 0:
+        qtd_de_moedas = len(Util.obter_lista_de_moedas())
 
-            qtd_de_moedas = len(Util.obter_lista_de_moedas())
+        corretoraParte.atualizarSaldo()
+        corretoraContraparte.atualizarSaldo()
 
-            corretoraParte.atualizarSaldo()
-            corretoraContraparte.atualizarSaldo()
-
-            saldoTotalBRL = corretoraParte.saldoBRL + corretoraContraparte.saldoBRL
+        saldoTotalBRL = corretoraParte.saldoBRL + corretoraContraparte.saldoBRL
+        
+        #corretoraParte tem que ser Brasil, pq la a liquidez é menor e mais facil de fazer leilão
+        if ((corretoraParte.precoVenda+0.01) <= 0.99 * corretoraContraparte.precoVenda):# o preço que eu posso comprar é menor doq o preço que posso vender
             
-            #corretoraParte tem que ser Brasil, pq la a liquidez é menor e mais facil de fazer leilão
-            if ((corretoraParte.precoVenda+0.01) <= 0.99 * corretoraContraparte.precoVenda):# o preço que eu posso comprar é menor doq o preço que posso vender
-                
-                gostaria_de_comprar = corretoraParte.saldoBRL/(qtd_de_moedas*corretoraParte.precoVenda+0.01)
-                maximo_que_consigo_zerar = corretoraContraparte.saldoCrypto/4
-                qtdNegociada = min(gostaria_de_comprar,maximo_que_consigo_zerar)
-                
-                if qtdNegociada > 0:
+            gostaria_de_comprar = corretoraParte.saldoBRL/(qtd_de_moedas*corretoraParte.precoVenda+0.01)
+            maximo_que_consigo_zerar = corretoraContraparte.saldoCrypto/4
+            qtdNegociada = min(gostaria_de_comprar,maximo_que_consigo_zerar)
+            
+            if qtdNegociada > 0:
 
-                    if corretoraContraparte.saldoBRL < (saldoTotalBRL/8): #eh pra ser deseperado aqui, tenho menos em reais na mercado doq um decimo do totalbrl
-                        #quando estou desesperado uso a regra do pnl zero
-                        corretoraParte.precoCompra = corretoraContraparte.precoVenda*0.99-0.01
-                        CompraCorretoraParte = corretoraParte.enviarOrdemCompra(qtdNegociada, 'limited') #ta certo isso lucas?
-                        logList['idOrdem'] = CompraCorretoraParte['data']['id']
+                if corretoraContraparte.saldoBRL < (saldoTotalBRL/8): #eh pra ser deseperado aqui, tenho menos em reais na mercado doq um decimo do totalbrl
+                    #quando estou desesperado uso a regra do pnl zero
+                    corretoraParte.precoCompra = corretoraContraparte.precoVenda*0.99-0.01
+                    CompraCorretoraParte = corretoraParte.enviarOrdemCompra(qtdNegociada, 'limited') #ta certo isso lucas?
+                    logList['idOrdem'] = CompraCorretoraParte['data']['id']
 
-                    else:#todos outros casos
-                        corretoraParte.precoCompra = corretoraParte.precoVenda + 0.01 #quando não estou desesperado, uso a regra do um centavo
-                        CompraCorretoraParte = corretoraParte.enviarOrdemCompra(qtdNegociada, 'limited') #ta certo isso lucas?
-                        logList['idOrdem'] = CompraCorretoraParte['data']['id']
+                else:#todos outros casos
+                    corretoraParte.precoCompra = corretoraParte.precoVenda + 0.01 #quando não estou desesperado, uso a regra do um centavo
+                    CompraCorretoraParte = corretoraParte.enviarOrdemCompra(qtdNegociada, 'limited') #ta certo isso lucas?
+                    logList['idOrdem'] = CompraCorretoraParte['data']['id']
 
         return logList
 
