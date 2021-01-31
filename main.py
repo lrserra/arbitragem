f5e7e873566ea0d4ae5b25b061a781973c2dea6b
rafael_bit <rafael.verde88@gmail.com>
2021-01-31 11:59:58 -0300

    um pouco mais de log no leilao

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ while hour <= 720:
 
                     pnl = round(((vendi_a * 0.998) - (comprei_a * 1.007)) * quantidade,2)
 
-                    logging.warning('operou leilao de compra de {}! + {}brl de pnl (compra de {}{} @{} e venda a @{})'.format(moeda,pnl,quantidade,moeda,comprei_a,vendi_a))
+                    logging.warning('operou leilao de compra de {}! + {}brl de pnl (compra de {}{} @{} na {} e venda a @{} na {})'.format(moeda,pnl,quantidade,moeda,comprei_a,CorretoraMaisLiquida.nome,vendi_a,CorretoraMenosLiquida.nome))
                     CorretoraMaisLiquida.atualizar_saldo()
                     CorretoraMenosLiquida.atualizar_saldo()
                     dict_leilao_compra[moeda]['ordem'] = Ordem() #reinicia as ordens
@@ -99,7 +99,7 @@ while hour <= 720:
 
                     pnl = round(((vendi_a*0.993)-(comprei_a*1.002)) * quantidade,2)
 
-                    logging.warning('operou leilao de venda de {}! + {}brl de pnl (venda de {}{} @{} e compra a @{})'.format(moeda,pnl,quantidade,moeda,vendi_a,comprei_a))
+                    logging.warning('operou leilao de venda de {}! + {}brl de pnl (venda de {}{} @{} na {} e compra a @{} na {})'.format(moeda,pnl,quantidade,moeda,vendi_a,CorretoraMaisLiquida.nome,comprei_a,CorretoraMenosLiquida.nome))
                     CorretoraMaisLiquida.atualizar_saldo()
                     CorretoraMenosLiquida.atualizar_saldo() 
                     dict_leilao_venda[moeda]['ordem'] = Ordem() #reinicia as ordens   
