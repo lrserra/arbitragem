8bc003b0469b784240095e39afa0cb92b4099c3e
rafael_bit <rafael.verde88@gmail.com>
2021-02-15 09:48:01 -0300

    arrumei pnl do leilao

--- corretora.py
+++ corretora.py
@@ -75,6 +75,53 @@ class Corretora:
                 self.book = BitcoinTrade(self.ativo).obterBooks()
         except Exception as erro:
             raise Exception(erro)
+    
+    def obter_preco_medio_de_venda(self,qtd_a_vender):
+        try:
+            if self.nome == 'MercadoBitcoin':
+                precos = self.book
+                qtd_vendida = 0
+                preco_medio = 0
+                linha = 0
+                lista_de_precos = precos['bids']
+                while qtd_vendida <qtd_a_vender:
+                    vou_vender_nessa_linha = min(lista_de_precos[linha][1],qtd_a_vender-qtd_vendida)
+                    qtd_vendida += vou_vender_nessa_linha
+                    preco_medio += lista_de_precos[linha][0]*vou_vender_nessa_linha
+                    #print('vendi {} a {} na linha {}'.format(vou_vender_nessa_linha,lista_de_precos[linha][0],linha))
+                    linha +=1
+                return round(preco_medio/qtd_vendida,4)
+            
+            elif self.nome == 'BrasilBitcoin':
+                pass
+            elif self.nome == 'BitcoinTrade':
+                pass   
+        except Exception as erro:
+            raise Exception(erro)
+    
+    def obter_preco_medio_de_compra(self,qtd_a_comprar):
+        try:
+            if self.nome == 'MercadoBitcoin':
+                precos = self.book
+                qtd_a_comprar = 2
+                qtd_comprada = 0
+                preco_medio = 0
+                linha = 0
+                lista_de_precos = precos['asks']
+                while qtd_comprada <qtd_a_comprar:
+                        
+                        vou_comprar_nessa_linha = min(lista_de_precos[linha][1],qtd_a_comprar-qtd_comprada)
+                        qtd_comprada += vou_comprar_nessa_linha
+                        preco_medio += lista_de_precos[linha][0]*vou_comprar_nessa_linha
+                        linha +=1
+                return round(preco_medio/qtd_comprada,4)
+            
+            elif self.nome == 'BrasilBitcoin':
+                pass
+            elif self.nome == 'BitcoinTrade':
+                pass   
+        except Exception as erro:
+            raise Exception(erro)
 
     def obter_financeiro_corretagem_compra_por_corretora(self, quantidade_compra = 0):
         if quantidade_compra == 0:
