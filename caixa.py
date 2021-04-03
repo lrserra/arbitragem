2ea7f8292a30cb935de8cce0c4eb51eba03fa635
rafael_bit <rafael.verde88@gmail.com>
2021-04-03 11:01:59 -0300

    tb zera o pnl em cripto

--- caixa.py
+++ caixa.py
@@ -211,7 +211,7 @@ if __name__ == "__main__":
     CorretoraMenosLiquida = Corretora(corretora_menos_liquida)
 
     Caixa.atualiza_saldo_inicial(lista_de_moedas,CorretoraMaisLiquida,CorretoraMenosLiquida)
-    #Caixa.zera_o_pnl_em_cripto(CorretoraMaisLiquida,CorretoraMenosLiquida,'',False)
+    Caixa.zera_o_pnl_em_cripto(CorretoraMaisLiquida,CorretoraMenosLiquida,'',False)
     
     CorretoraMaisLiquida.atualizar_saldo()
     CorretoraMenosLiquida.atualizar_saldo()
