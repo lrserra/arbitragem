b7194a53efaedaa4588a23047ed842bb3835801d
lrserra <lucasribeiroserra@gmail.com>
2021-09-16 00:05:55 -0300

    Add trading para validar Binance

--- trading.py
+++ trading.py
@@ -1,17 +1,20 @@
 from uteis.util import Util
-from uteis.ordem import Ordem
+from uteis.googleSheets import GoogleSheets
 from uteis.corretora import Corretora
+import time
 
 corrBinance = Corretora('Binance')
+corrBrasilBTC = Corretora('BrasilBitcoin')
 
+google_sheets = GoogleSheets()
 
-ordem_venda_limitada = Ordem()
+while True:
+    lista_de_moedas = ['ada', 'doge', 'usdt', 'btc', 'eth', 'xrp', 'ltc', 'bch']
+    
+    for moeda in lista_de_moedas:
+        corrBinance.book.obter_ordem_book_por_indice(moeda, Util.CCYBRL())
+        corrBrasilBTC.book.obter_ordem_book_por_indice(moeda, Util.CCYBRL())
 
-ordem_venda_limitada.quantidade_enviada = 4
-ordem_venda_limitada.preco_enviado = 7
-ordem_venda_limitada.tipo_ordem = 'market'
-
-ret_compra = corrBinance.enviar_ordem_venda(ordem_venda_limitada, Util.CCYXRP(), Util.CCYBRL())
-
-
-
+        google_sheets.escrever("King of All In", 'Binance', [moeda,corrBinance.nome,corrBinance.book.preco_compra,corrBinance.book.preco_venda,corrBrasilBTC.nome,corrBrasilBTC.book.preco_compra,corrBrasilBTC.book.preco_venda])
+    
+    time.sleep(60)
