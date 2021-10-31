c5f25d4b63b0eb461a126f6b5b0be79ffdf135bd
rafael_bit <rafael.verde88@gmail.com>
2021-10-31 09:56:18 -0300

    add doge usdt

--- caixa.py
+++ caixa.py
@@ -65,7 +65,7 @@ class Caixa:
         preco_venda['usdc']=CorretoraMaisLiquida.book.preco_venda
 
         logging.warning('caixa vai enviar saldo para o google')
-        GoogleSheets().escrever_saldo([saldo['brl'],saldo['btc'],saldo['eth'],saldo['xrp'],saldo['ltc'],saldo['bch'],saldo['ada'],saldo['btc']*preco_venda['btc'],saldo['eth']*preco_venda['eth'],saldo['xrp']*preco_venda['xrp'],saldo['ltc']*preco_venda['ltc'],saldo['bch']*preco_venda['bch'],saldo['ada']*preco_venda['ada'],CorretoraMaisLiquida.saldo['brl'],CorretoraMaisLiquida.saldo['btc'],CorretoraMaisLiquida.saldo['eth'],CorretoraMaisLiquida.saldo['xrp'],CorretoraMaisLiquida.saldo['ltc'],CorretoraMaisLiquida.saldo['bch'],CorretoraMaisLiquida.saldo['ada'], Util.excel_date(datetime.now()),preco_venda['usdc']])
+        GoogleSheets().escrever_saldo([saldo['brl'],saldo['btc'],saldo['eth'],saldo['xrp'],saldo['ltc'],saldo['bch'],saldo['ada'],saldo['usdt'],saldo['doge'],saldo['btc']*preco_venda['btc'],saldo['eth']*preco_venda['eth'],saldo['xrp']*preco_venda['xrp'],saldo['ltc']*preco_venda['ltc'],saldo['bch']*preco_venda['bch'],saldo['ada']*preco_venda['ada'],saldo['usdt']*preco_venda['usdt'],saldo['doge']*preco_venda['doge'],CorretoraMaisLiquida.saldo['brl'],CorretoraMaisLiquida.saldo['btc'],CorretoraMaisLiquida.saldo['eth'],CorretoraMaisLiquida.saldo['xrp'],CorretoraMaisLiquida.saldo['ltc'],CorretoraMaisLiquida.saldo['bch'],CorretoraMaisLiquida.saldo['ada'],CorretoraMaisLiquida.saldo['usdt'],CorretoraMaisLiquida.saldo['doge'], Util.excel_date(datetime.now()),preco_venda['usdc']])
 
 
     def zera_o_pnl_de_todas_moedas(self,lista_de_moedas,CorretoraMaisLiquida:Corretora,CorretoraMenosLiquida:Corretora,atualizar_saldo=True):
