bd986b2ef7513945a000cd4a835aa00dfdebbebe
Rafael <rafael.verde88@gmail.com>
2022-04-12 11:15:48 -0300

    usando ip list

--- watchdog.py
+++ watchdog.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os, sys, time
+import urllib.request
 
 from uteis.logger import Logger
 
@@ -29,10 +30,18 @@ def get_temperature():
         cpu_temp = 0
     return cpu_temp
 
+def get_ip():
+    try:
+        my_ip = urllib.request.urlopen('https://api.ipify.org/').read().decode('utf8')
+    except:
+        my_ip = ''
+    return my_ip
+
 settings_client = Settings()
 google_client = Google()
 instance = settings_client.retorna_campo_de_json('rasp','instance')
 planilha = settings_client.retorna_campo_de_json('rasp','sheet_name')
+my_ip_list = settings_client.retorna_campo_de_json_como_lista('rasp','ip_list')
 
 Logger.cria_arquivo_log('WatchDog')
 Logger.loga_info('iniciando script watchdog...')
@@ -40,14 +49,18 @@ Logger.loga_info('iniciando script watchdog...')
 while True:
 
     try:    
-        update_range =[[get_now_time(),
-                        get_update_time('Arbitragem.log'),
-                        get_update_time('Leilao.log'),
-                        get_temperature()]]
+        my_external_ip = get_ip()
+        if my_external_ip in my_ip_list:
+            update_range =[[get_now_time(),
+                            get_update_time('Arbitragem.log'),
+                            get_update_time('Leilao.log'),
+                            get_temperature()]]
 
-        google_client.update(planilha,'settings','monitor{}'.format(instance),update_range)
-        Logger.loga_info('updated!')
-        time.sleep(180)
+            google_client.update(planilha,'settings','monitor{}'.format(instance),update_range)
+            Logger.loga_info('updated!')
+            time.sleep(180)
+        else:
+            Logger.loga_warning('my ip {} not in my ip list {}'.format(my_external_ip,my_ip_list))
     except Exception as err:
         Logger.loga_erro('main','watchdog',err)
         time.sleep(60)
